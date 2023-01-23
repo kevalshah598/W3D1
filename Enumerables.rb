5a821ce3e28141b09ac3d81f5f93133606535ef4
Naoko Sugawara <infoseek122@gmail.com>
2023-01-23 17:24:08 -0500

    my_select

--- Enumerables.rb
+++ Enumerables.rb
@@ -7,19 +7,34 @@ class Array
         end
         return self
     end
+
+    def my_select(&bloc)
+        new_ary = []
+        self.my_each do |ele|
+            if bloc.call(ele)
+                new_ary << ele
+            end
+        end
+        new_ary
+    end
+
+    # a = [1, 2, 3]
+    # p a.my_select { |num| num > 1 } # => [2, 3]
+    # p a.my_select { |num| num == 4 } # => []
+
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
-   # => 1
-       2
-       3
-       1
-       2
-       3
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
+#    # => 1
+#        2
+#        3
+#        1
+#        2
+#        3
    
-   p return_value  # => [1, 2, 3]
+#    p return_value  # => [1, 2, 3]
