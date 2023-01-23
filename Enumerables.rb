c046fde2faedb838751c37af3a183674d6a93a9c
Keval Shah <kevalshah598@gmail.com>
2023-01-23 17:09:11 -0500

    my_each

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,3 +1,25 @@
 class Array
-    
+    def my_each(&bloc)
+        i = 0
+        while i < self.length
+            bloc.call(self[i])
+        i += 1
+        end
+        return self
+    end
 end
+
+# calls my_each twice on the array, printing all the numbers twice.
+return_value = [1, 2, 3].my_each do |num|
+    puts num
+   end.my_each do |num|
+    puts num
+   end
+   # => 1
+       2
+       3
+       1
+       2
+       3
+   
+   p return_value  # => [1, 2, 3]
